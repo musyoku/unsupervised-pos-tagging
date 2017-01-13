de348b2f0e674b55a8172cc1157a5f4a714aa139
musyoku <musyoku@users.noreply.github.com>
2017-01-14 01:11:56 +0900

    :v:

--- bayesian-hmm/core/bhmm.h
+++ bayesian-hmm/core/bhmm.h
@@ -7,12 +7,26 @@ public:
 	int _num_phrases;	// 品詞数
 	int _num_words;		// 単語数
 	int*** _trigram_table;	// 3-gramテーブル
-	int** _marginal_counts_unigram;	// sum _trigram_table[t_-2][t_-1][.]
-	int* _marginal_counts_bigram_unigram;	// sum _trigram_table[t_-2][.][.]
+	int** _marginal_counts_tri_bi;	// sum _trigram_table[t_-2][t_-1][.]
+	int* _marginal_counts_tri;	// sum _trigram_table[t_-2][.][.]
 	HMM(int num_phrases, int num_words){
 		_num_phrases = num_phrases;
 		_num_words = num_words;
 		init_table();
+	}
+	~HMM(){
+		for(int tri_phrase = 0;tri_phrase < _num_phrases;tri_phrase++){
+			for(int bi_phrase = 0;bi_phrase < _num_phrases;bi_phrase++){
+				free(_trigram_table[tri_phrase][bi_phrase])
+			}
+			free(_trigram_table[tri_phrase])
+		}
+		free(_trigram_table);
+		for(int bi_phrase = 0;bi_phrase < _num_phrases;bi_phrase++){
+			free(_marginal_counts_tri_bi)
+		}
+		free(_marginal_counts_tri_bi)
+		free(_marginal_counts_tri);
 	}
 	void init_table(){
 		_trigram_table = (int***)malloc(_num_phrases * sizeof(int**));
@@ -22,26 +36,17 @@ public:
 				_trigram_table[tri_phrase][bi_phrase] = (int*)calloc(_num_phrases, sizeof(int))
 			}
 		}
-		_marginal_counts_unigram = (int**)malloc(_num_phrases * sizeof(int*));
+		_marginal_counts_tri_bi = (int**)malloc(_num_phrases * sizeof(int*));
 		for(int bi_phrase = 0;bi_phrase < _num_phrases;bi_phrase++){
-			_marginal_counts_unigram = (int*)calloc(_num_phrases, sizeof(int));
+			_marginal_counts_tri_bi = (int*)calloc(_num_phrases, sizeof(int));
 		}
+		_marginal_counts_tri = (int*)calloc(_num_phrases, sizeof(int));
 	}
 	int get_marginal_count_unigram(int tri_phrase, int bi_phrase){
-		int sum_counts = 0;
-		int** table = _trigram_table[tri_phrase][bi_phrase];
-		for(int uni_phrase = 0;uni_phrase < _num_phrases;uni_phrase++){
-			sum_counts += table[uni_phrase];
-		}
-		return sum_counts;
+		return _marginal_counts_tri_bi[tri_phrase][bi_phrase];
 	}
 	int get_marginal_count_bigram_unigram(int tri_phrase){
-		int sum_counts = 0;
-		int** table = _trigram_table[tri_phrase][bi_phrase];
-		for(int uni_phrase = 0;uni_phrase < _num_phrases;uni_phrase++){
-			sum_counts += table[uni_phrase];
-		}
-		return sum_counts;
+		return _marginal_counts_tri[tri_phrase];
 	}
 };
 
