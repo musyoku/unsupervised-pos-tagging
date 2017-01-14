4f0cffa49b3a76e70a495d485462aecea5ff0124
musyoku <musyoku@users.noreply.github.com>
2017-01-15 00:38:55 +0900

    :tired_face:

--- bayesian-hmm/core/bhmm.h
+++ bayesian-hmm/core/bhmm.h
@@ -23,7 +23,7 @@ public:
 	int** _bigram_counts;	// 品詞2-gramのカウント
 	int* _unigram_counts;	// 品詞1-gramのカウント
 	int* _word_types_for_tag;	// 同じ品詞の単語の種類
-	map<pair<int, int>, int> _tag_word_counts;	// 品詞と単語のペアの出現頻度
+	map<int, map<int, int>> _tag_word_counts;	// 品詞と単語のペアの出現頻度
 	double* _sampling_table;	// キャッシュ
 	double _alpha;
 	double _beta;
@@ -108,8 +108,22 @@ public:
 		for(int data_index = 0;data_index < dataset.size();data_index++){
 			vector<Word*> &line = dataset[data_index];
 			// pos < 2
+			// <bos>2つ
 			_bigram_counts[line[0]->tag_id][line[1]->tag_id] += 1;
+			_unigram_counts[line[0]->tag_id] += 1;
+			_unigram_counts[line[1]->tag_id] += 1;
+			word_set.insert(line[0]->word_id);
 			word_set.insert(line[1]->word_id);
+			auto newly_inserted = tag_word_set.insert(std::make_pair(line[0]->tag_id, line[0]->word_id));
+			if(newly_inserted.second == true){
+				_word_types_for_tag[line[0]->tag_id] += 1;
+			}
+			increment_tag_word_count(line[0]->tag_id, line[0]->word_id);
+			newly_inserted = tag_word_set.insert(std::make_pair(line[1]->tag_id, line[1]->word_id));
+			if(newly_inserted.second == true){
+				_word_types_for_tag[line[1]->tag_id] += 1;
+			}
+			increment_tag_word_count(line[1]->tag_id, line[1]->word_id);
 			// line.size() - 2 > pos >= 2
 			for(int pos = 2;pos < line.size() - 2;pos++){	// 3-gramなので3番目から.
 				Word* word = line[pos];
@@ -119,17 +133,41 @@ public:
 				// 単語の種類数をカウント
 				word_set.insert(word->word_id);
 				// 同じタグの単語集合をカウント
-				auto pair = std::make_pair(word->tag_id, word->word_id);
-				auto itr = tag_word_set.find(pair);
-				if(itr == tag_word_set.end()){
-					tag_word_set.insert(pair);
-					_word_types_for_tag[word->tag_id] += 1;
-				}
+				increment_tag_word_count(word->tag_id, word->word_id);
+				// auto pair = std::make_pair(word->tag_id, word->word_id);
+				// newly_inserted = tag_word_set.insert(pair);
+				// if(newly_inserted.second == true){
+				// 	_word_types_for_tag[word->tag_id] += 1;
+				// 			_tag_word_counts[word->tag_id][word->word_id] += 1;
+				// }
 			}
 			// pos >= line.size() - 2
-			_bigram_counts[line[line.size() - 3]->tag_id][line[line.size() - 2]->tag_id] += 1;
-			_trigram_counts[line[line.size() - 4]->tag_id][line[line.size() - 3]->tag_id][line[line.size() - 2]->tag_id] += 1;
-			_trigram_counts[line[line.size() - 3]->tag_id][line[line.size() - 2]->tag_id][line[line.size() - 1]->tag_id] += 1;
+			// <eos>2つ
+			int end_index = line.size() - 1;
+			int t_end = line[end_index]->tag_id;
+			int t_end_1 = line[end_index - 1]->tag_id;
+			int t_end_2 = line[end_index - 2]->tag_id;
+			int t_end_3 = line[end_index - 3]->tag_id;
+			_unigram_counts[t_end] += 1;
+			_unigram_counts[t_end_1] += 1;
+			_bigram_counts[t_end_2][t_end_1] += 1;
+			_bigram_counts[t_end_1][t_end] += 1;
+			_trigram_counts[t_end_3][t_end_2][t_end_1] += 1;
+			_trigram_counts[t_end_2][t_end_1][t_end] += 1;
+			int w_end = line[end_index]->word_id;
+			int w_end_1 = line[end_index - 1]->word_id;
+			word_set.insert(w_end);
+			word_set.insert(w_end_1);
+			newly_inserted = tag_word_set.insert(std::make_pair(t_end, w_end));
+			if(newly_inserted.second == true){
+				_word_types_for_tag[t_end] += 1;
+			}
+			increment_tag_word_count(t_end, w_end);
+			newly_inserted = tag_word_set.insert(std::make_pair(t_end_1, w_end_1));
+			if(newly_inserted.second == true){
+				_word_types_for_tag[t_end_1] += 1;
+			}
+			increment_tag_word_count(t_end_1, w_end_1);
 		}
 		_num_words = word_set.size();
 		c_printf("[*]%s\n", (boost::format("単語数: %d - 行数: %d") % _num_words % dataset.size()).str().c_str());
@@ -140,34 +178,55 @@ public:
 		_unigram_counts[uni_word->tag_id] += 1;
 	}
 	void increment_tag_word_count(int tag_id, int word_id){
-		auto pair = std::make_pair(tag_id, word_id);
-		auto itr = _tag_word_counts.find(pair);
-		if(itr == _tag_word_counts.end()){
-			_tag_word_counts[pair] = 1;
+		map<int, int> &word_counts = _tag_word_counts[tag_id];
+		auto itr = word_counts.find(word_id);
+		if(itr == word_counts.end()){
+			word_counts[word_id] = 1;
 			return;
 		}
-		_tag_word_counts[pair] += 1;
+		itr->second += 1;
 	}
 	void decrement_tag_word_count(int tag_id, int word_id){
-		auto pair = std::make_pair(tag_id, word_id);
-		auto itr = _tag_word_counts.find(pair);
-		if(itr == _tag_word_counts.end() || itr->second < 1){
+		map<int, int> &word_counts = _tag_word_counts[tag_id];
+		auto itr = word_counts.find(word_id);
+		if(itr == word_counts.end()){
 			c_printf("[R]%s [*]%s", "エラー", "品詞-単語ペアのカウントが正しく実装されていません.");
 			exit(1);
 		}
-		_tag_word_counts[pair] -= 1;
+		itr->second -= 1;
+		if(itr->second <= 0){
+			word_counts.erase(itr);
+		}
 	}
 	int get_count_for_tag_word(int tag_id, int word_id){
-		auto pair = std::make_pair(tag_id, word_id);
-		auto itr = _tag_word_counts.find(pair);
-		if(itr == _tag_word_counts.end()){
+		map<int, int> &word_counts = _tag_word_counts[tag_id];
+		auto itr = word_counts.find(word_id);
+		if(itr == word_counts.end()){
 			return 0;
 		}
 		return itr->second;
 	}
+	int get_word_types_for_tag(int tag_id){
+		map<int, int> &word_counts = _tag_word_counts[tag_id];
+		return word_counts.size();
+	}
+	// デバッグ用
+	void check_counts(){
+		int sum = 0;
+		for(int tag = 0;tag < _num_tags;tag++){
+			sum += _word_types_for_tag[tag];
+		}
+		assert(sum >= _num_words);
+	}
 	// in:  t_{i-2},t_{i-1},t_i,t_{i+1},t_{i+2},w_i
 	// out: void
 	void add_tag_to_model_parameters(int _t_i_2, int _t_i_1, int t_i, int t_i_1, int t_i_2, int w_i){
+				// cout << "add:" << _t_i_2 << "," << _t_i_1 << "," << t_i << "," << t_i_1 << "," << t_i_2 << "," << w_i << endl;
+				// dump_trigram_counts();
+				// dump_bigram_counts();
+				// dump_unigram_counts();
+				// dump_word_types();
+
 		// 1-gram
 		_unigram_counts[t_i] += 1;
 		// 2-gram
@@ -178,13 +237,17 @@ public:
 		_trigram_counts[_t_i_1][t_i][t_i_1] += 1;
 		_trigram_counts[t_i][t_i_1][t_i_2] += 1;
 		// 品詞-単語ペア
-		auto pair = std::make_pair(t_i, w_i);
-		auto itr = _tag_word_counts.find(pair);
-		_tag_word_counts[pair] += 1;
+		increment_tag_word_count(t_i, w_i);
 	}
 	// in:  t_{i-2},t_{i-1},t_i,t_{i+1},t_{i+2},w_i
 	// out: void
 	void remove_tag_from_model_parameters(int _t_i_2, int _t_i_1, int t_i, int t_i_1, int t_i_2, int w_i){
+				// cout << "remove:" << _t_i_2 << "," << _t_i_1 << "," << t_i << "," << t_i_1 << "," << t_i_2 << "," << w_i << endl;
+				// dump_trigram_counts();
+				// dump_bigram_counts();
+				// dump_unigram_counts();
+				// dump_word_types();
+
 		// 1-gram
 		_unigram_counts[t_i] -= 1;
 		assert(_unigram_counts[t_i] >= 0);
@@ -201,12 +264,26 @@ public:
 		_trigram_counts[t_i][t_i_1][t_i_2] -= 1;
 		assert(_trigram_counts[t_i][t_i_1][t_i_2] >= 0);
 		// 品詞-単語ペア
-		auto pair = std::make_pair(t_i, w_i);
-		auto itr = _tag_word_counts.find(pair);
-		assert(itr != _tag_word_counts.end());
-		_tag_word_counts[pair] -= 1;
-		assert(itr->second >= 0);
+		decrement_tag_word_count(t_i, w_i);
 	}
+	// デバッグ用
+	// void remove_all_tag_from_model_parameters(vector<Word*> &line){
+	// 	for(int pos = 2;pos < line.size() - 2;pos++){	// <bos>と<eos>の内側だけ考える
+	// 		int t_i = line[pos]->tag_id;
+	// 		int w_i = line[pos]->word_id;
+	// 		int t_i_1 = line[pos + 1]->tag_id;
+	// 		int t_i_2 = line[pos + 2]->tag_id;
+	// 		int _t_i_1 = line[pos - 1]->tag_id;
+	// 		int _t_i_2 = line[pos - 2]->tag_id;
+	// 		// t_iをモデルパラメータから除去
+	// 		// cout << "remove all:" << _t_i_2 << "," << _t_i_1 << "," << t_i << "," << t_i_1 << "," << t_i_2 << "," << w_i << endl;
+	// 		// dump_trigram_counts();
+	// 		// dump_bigram_counts();
+	// 		// dump_unigram_counts();
+	// 		// dump_word_types();
+	// 		remove_tag_from_model_parameters(_t_i_2, _t_i_1, t_i, t_i_1, t_i_2, w_i);
+	// 	}
+	// }
 	void perform_gibbs_sampling_with_line(vector<Word*> &line){
 		if(_sampling_table == NULL){
 			_sampling_table = (double*)malloc(_num_tags * sizeof(double));
@@ -225,21 +302,21 @@ public:
 			int new_t_i = 0;
 			for(int tag = 0;tag < _num_tags;tag++){
 				_sampling_table[tag] = 1;
-				double n_t_i_w_i = get_count_for_tag_word(tag, t_i);
-				double n_t_i = _unigram_counts[t_i];
-				double W_t_i = _word_types_for_tag[t_i];
-				double n_t_2_1_i = _trigram_counts[_t_i_2][_t_i_1][t_i];
+				double n_t_i_w_i = get_count_for_tag_word(tag, w_i);
+				double n_t_i = _unigram_counts[tag];
+				double W_t_i = get_word_types_for_tag(tag);
+				double n_t_2_1_i = _trigram_counts[_t_i_2][_t_i_1][tag];
 				double n_t_2_1 = _bigram_counts[_t_i_2][_t_i_1];
-				double n_t_1_i_1 = _trigram_counts[_t_i_1][t_i][t_i_1];
-				double n_t_1_i = _bigram_counts[_t_i_1][t_i];
-				double I_2_1_i_1 = (_t_i_2 == _t_i_1 == t_i == t_i_1) ? 1 : 0;
-				double I_2_1_i = (_t_i_2 == _t_i_1 == t_i) ? 1 : 0;
-				double n_t_i_1_2 = _trigram_counts[t_i][t_i_1][t_i_2];
-				double n_t_i_1 = _bigram_counts[t_i][t_i_1];
-				double I_2_i_2_1_1 = (_t_i_2 == t_i == t_i_2 && _t_i_1 == t_i_1) ? 1 : 0;
-				double I_1_i_1_2 = (_t_i_1 == t_i == t_i_1 == t_i_2) ? 1 : 0;
-				double I_2_i_1_1 = (_t_i_2 == t_i && _t_i_1 == t_i_1) ? 1 : 0;
-				double I_1_i_1 = (_t_i_1 == t_i == t_i_1) ? 1 : 0;
+				double n_t_1_i_1 = _trigram_counts[_t_i_1][tag][t_i_1];
+				double n_t_1_i = _bigram_counts[_t_i_1][tag];
+				double I_2_1_i_1 = (_t_i_2 == _t_i_1 == tag == t_i_1) ? 1 : 0;
+				double I_2_1_i = (_t_i_2 == _t_i_1 == tag) ? 1 : 0;
+				double n_t_i_1_2 = _trigram_counts[tag][t_i_1][t_i_2];
+				double n_t_i_1 = _bigram_counts[tag][t_i_1];
+				double I_2_i_2_1_1 = (_t_i_2 == tag == t_i_2 && _t_i_1 == t_i_1) ? 1 : 0;
+				double I_1_i_1_2 = (_t_i_1 == tag == t_i_1 == t_i_2) ? 1 : 0;
+				double I_2_i_1_1 = (_t_i_2 == tag && _t_i_1 == t_i_1) ? 1 : 0;
+				double I_1_i_1 = (_t_i_1 == tag == t_i_1) ? 1 : 0;
 				_sampling_table[tag] *= (n_t_i_w_i + _beta) / (n_t_i + W_t_i * _beta);
 				_sampling_table[tag] *= (n_t_2_1_i + _alpha) / (n_t_2_1 + _num_tags * _alpha);
 				_sampling_table[tag] *= (n_t_1_i_1 + I_2_1_i_1 + _alpha) / (n_t_1_i + I_2_1_i + _num_tags * _alpha);
@@ -267,8 +344,7 @@ public:
 		int max_count = 0;
 		int most_co_occurring_tag_id = 0;
 		for(int tag = 0;tag < _num_tags;tag++){
-			auto pair = std::make_pair(tag, word_id);
-			int count = _tag_word_counts[pair];
+			int count = get_count_for_tag_word(tag, word_id);
 			if(count > max_count){
 				max_count = count;
 				most_co_occurring_tag_id = tag;
@@ -296,6 +372,20 @@ public:
 		for(int uni_tag = 0;uni_tag < _num_tags;uni_tag++){
 			cout << (boost::format("1-gram [%d] = %d") % uni_tag % _unigram_counts[uni_tag]).str() << endl;
 		}
+	}
+	void dump_word_types(){
+		for(int tag = 0;tag < _num_tags;tag++){
+			cout << tag << ": " << get_word_types_for_tag(tag) << endl;
+		}
+
+
+			// for(auto &tag: _tag_word_counts){
+			// 	map<int, int> &words = tag.second;
+			// 	cout << tag.first << "<<" << endl;
+			// 	for(auto &word: words){
+			// 		cout << word.first << ":+:" << word.second << endl;
+			// 	}
+			// }
 	}
 };
 
