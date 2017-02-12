e753de94320e1112b3aa597d372fbd62fe5729b9
musyoku <musyoku@users.noreply.github.com>
2017-02-13 03:04:58 +0900

    :v:

--- infinite-tree-hmm/core/node.h
+++ infinite-tree-hmm/core/node.h
@@ -1,5 +1,6 @@
 #ifndef _node_
 #define _node_
+#include <cassert>
 #include <unordered_map>
 using namespace std;
 using namespace boost;
@@ -98,9 +99,87 @@ public:
 		if(_parent != NULL){
 		}
 	}
+	int get_vertical_stop_count(){
+		return get_vertical_stop_count_with_id(_identifier);
+	}
+	int get_vertical_stop_count_with_id(int identifier){
+		auto itr = _stop_count_v.find(identifier);
+		if(itr == _stop_count_v.end()){
+			return 0;
+		}
+		return itr->second;
+	}
+	int get_vertical_pass_count(){
+		return get_vertical_pass_count_with_id(_identifier);
+	}
+	int get_vertical_pass_count_with_id(int identifier){
+		auto itr = _pass_count_v.find(identifier);
+		if(itr == _pass_count_v.end()){
+			return 0;
+		}
+		return itr->second;
+	}
+	int get_horizontal_stop_count(){
+		return get_horizontal_stop_count_with_id(_identifier);
+	}
+	int get_horizontal_stop_count_with_id(int identifier){
+		auto itr = _stop_count_h.find(identifier);
+		if(itr == _stop_count_h.end()){
+			return 0;
+		}
+		return itr->second;
+	}
+	int get_horizontal_pass_count(){
+		return get_horizontal_pass_count_with_id(_identifier);
+	}
+	int get_horizontal_pass_count_with_id(int identifier){
+		auto itr = _pass_count_h.find(identifier);
+		if(itr == _pass_count_h.end()){
+			return 0;
+		}
+		return itr->second;
+	}
+	Table* get_vertical_table_with_id(int identifier){
+		auto itr = _table_v.find(identifier);
+		if(itr == _table_v.end()){
+			return NULL;
+		}
+		return itr->second;
+	}
+	Table* get_horizontal_table_with_id(int identifier){
+		auto itr = _table_h.find(identifier);
+		if(itr == _table_h.end()){
+			return NULL;
+		}
+		return itr->second;
+	}
 	// 縦の棒折り過程における、棒を折る比率の期待値を計算。論文中のコインの表が出る確率に相当
-	double compute_expectation_of_sbr_param_v(){
-		
+	double compute_expectation_of_sbr_param_v(double alpha){
+		vector<double> expectation_over_parents;
+		// トップレベルのノードから順に下りながら計算すると効率が良い
+		int num_parents = _depth_v;
+		for(int n = 0;n < num_parents;n++){
+			Node* target = _parent;
+			assert(target != NULL;)
+			for(step = 0;step < num_parents - n;step++){
+				target = target->_parent;
+				assert(target != NULL;)
+			}
+			// 親のTSSBにおけるこのノードの期待値なので自分のメソッドを呼ぶ
+			double expectation = _compute_expectation_of_sbr_param_v(alpha, target, expectation_over_parents);
+			expectation_over_parents.push_back(expectation);
+		}
+		return _compute_expectation_of_sbr_param_v(alpha, this, expectation_over_parents);
+	}
+	double _compute_expectation_of_sbr_param_v(double alpha, Node* target, vector<double> &expectation_over_parents){
+		int pass_count = get_vertical_pass_count(target->_identifier);
+		int stop_count = get_vertical_stop_count(target->_identifier);
+		if(target->_parent == NULL){
+			return (1.0 + stop_count) / (1.0 + alpha + stop_count + pass_count);
+		}
+		double v_parent = expectation_over_parents.back();
+		double sum_v_parents = std::accumulate(expectation_over_parents.begin(), expectation_over_parents.end(), 0.0);
+		return (alpha * v_parent + stop_count) / (alpha * (1.0 - sum_v_parents) + stop_count + pass_count);
 	}
 	// 縦の棒折り過程における、棒を折る比率をサンプリング。論文中のコインの表が出る確率に相当
 	double sample_sbr_param_v(){
@@ -117,6 +196,20 @@ public:
 	// TSSBでこのノードに止まる確率。
 	double compute_stop_probability(){
 
+	}
+	// 客を追加
+	void add_customer_to_vertical_crp(int customer_id){
+		
+	}
+	void add_customer_to_horizontal_crp(int customer_id){
+		
+	}
+	// 親のTSSBにおけるこのノードに代理客を追加
+	void add_customer_to_parent_tssb_vertical_crp(int customer_id){
+		
+	}
+	void add_customer_to_parent_tssb_horizontal_crp(int customer_id){
+		
 	}
 };
 int Node::_auto_increment = 0;
