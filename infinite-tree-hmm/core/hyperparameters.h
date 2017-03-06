120fd92faba68a0373fa48ba1569d7dc184d3c5e
musyoku <musyoku@users.noreply.github.com>
2017-03-07 03:36:02 +0900

    :v:

--- infinite-tree-hmm/core/hyperparameters.h
+++ infinite-tree-hmm/core/hyperparameters.h
@@ -3,15 +3,15 @@
 
 // これらのハイパーパラメータは実行前に一度だけ一様分布からサンプリングしていたのでminとmaxを設定する*1
 #define iTHMM_ALPHA_MIN 2
-#define iTHMM_ALPHA_MAX 3
-#define iTHMM_GAMMA_MIN 1
-#define iTHMM_GAMMA_MAX 2
+#define iTHMM_ALPHA_MAX 10
+#define iTHMM_GAMMA_MIN 0.1
+#define iTHMM_GAMMA_MAX 1
 #define iTHMM_LAMBDA_MIN 0.001	// 0以上1以下
 #define iTHMM_LAMBDA_MAX 0.05	// 0以上1以下
 // HTSSBで親の情報をどの程度受け継ぐかを制御するパラメータ
 // 論文では上のαと同じ記号が使われているが区別する必要がある
 #define iTHMM_STRENGTH_MIN 0.5
-#define iTHMM_STRENGTH_MAX 2
+#define iTHMM_STRENGTH_MAX 5
 
 // これらのハイパーパラメータは固定*2
 #define iTHMM_TAU_0 1.0
@@ -21,9 +21,6 @@
 // 実行中にサンプリングして正しい値を推定する
 #define HPYLM_D 0.2					// 0以上1未満. ディスカウント係数
 #define HPYLM_THETA 0.01			// 集中度
-// ルートノードでは単語の出力分布はフラットになってほしいので以下の固定値を用いる
-#define HPYLM_D_ROOT 0.2		// ルートノードでのディスカウント係数
-#define HPYLM_THETA_ROOT 100000		// ルートノードでの集中度
 // 以下は上のハイパーパラメータ2つを推定する時に使うハイパーパラメータ
 // 実行中常に固定値
 #define HPYLM_A 	1.0
@@ -31,7 +28,7 @@
 #define HPYLM_ALPHA 1.0
 #define HPYLM_BETA  1.0
 
-#define EPS 1e-12			// 本来これは0が望ましい
+#define EPS 1e-16	// 本来これは0が望ましい
 
 // *1 [Tree-Structured Stick Breaking for Hierarchical Data](https://hips.seas.harvard.edu/files/adams-tssb-nips-2010.pdf)
 // *2 [無限木構造隠れMarkovモデルによる階層的品詞の教師なし学習](http://chasen.org/~daiti-m/paper/nl226ithmm.pdf)
--- infinite-tree-hmm/test.cpp
+++ infinite-tree-hmm/test.cpp
@@ -680,9 +680,7 @@ void test29(){
 			cout << "lambda: " << model->_ithmm->_lambda << endl;
 			cout << "strength: " << model->_ithmm->_strength << endl;
 			cout << "log_Pdata: " << model->compute_log_Pdataset_test() << endl;
-			cout << "log2_Pdata: " << model->compute_log2_Pdataset_test() << endl;
-			cout << "PPL (train): " << model->compute_perplexity_train() << endl;
-			cout << "PPL (test): " << model->compute_perplexity_test() << endl;
+			cout << "PPL: " << model->compute_perplexity_test() << endl;
 			cout << "MH: " << model->_ithmm->_num_mh_acceptance / (double)(model->_ithmm->_num_mh_acceptance + model->_ithmm->_num_mh_rejection) << endl;;
 			for(int i = 0;i <= model->_ithmm->_current_max_depth;i++){
 				cout << "d[" << i << "] = " << model->_ithmm->_hpylm_d_m[i] << endl;
