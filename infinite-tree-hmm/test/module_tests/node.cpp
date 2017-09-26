a06b1e3d61aac49a56b3cc74b896c7ce4644aa3a
musyoku <musyoku@users.noreply.github.com>
2017-09-26 22:29:18 +0900

    :v:

--- infinite-tree-hmm/test/module_tests/node.cpp
+++ infinite-tree-hmm/test/module_tests/node.cpp
@@ -216,6 +216,144 @@ void test_add_customer_to_horizontal_crp_recursively(){
 	assert(child->_children[3]->_stop_count_h == 0);
 }
 
+void test_remove_customer_to_vertical_crp(){
+	Node* node = new Node();
+	double concentration = 0.2;
+	double g0 = 0.001;
+	bool new_table_generated;
+	for(int i = 0 ;i < 100;i++){
+		node->add_customer_to_vertical_crp(concentration, g0, new_table_generated);
+	}
+	for(int i = 0 ;i < 100;i++){
+		node->remove_customer_from_vertical_crp(new_table_generated);
+	}
+	for(int i = 0 ;i < 100;i++){
+		node->add_customer_to_vertical_crp(concentration, g0, new_table_generated);
+		node->remove_customer_from_vertical_crp(new_table_generated);
+	}
+	assert(node->_pass_count_v == 0);
+	assert(node->_pass_count_h == 0);
+	assert(node->_stop_count_v == 0);
+	assert(node->_stop_count_h == 0);
+	assert(node->_table_v->_num_customers == 0);
+}
+
+void test_remove_customer_to_vertical_crp_recursively(){
+	Node* parent = new Node();
+	parent->generate_child();
+	parent->generate_child();
+	Node* child = new Node(parent);
+	parent->add_child(child);
+	parent->generate_child();
+	double concentration = 0.2;
+	double g0 = 0.001;
+	bool new_table_generated;
+	for(int i = 0 ;i < 100;i++){
+		child->add_customer_to_vertical_crp(concentration, g0, new_table_generated);
+	}
+	child->generate_child();
+	child->generate_child();
+	Node* grandson = new Node(child);
+	child->add_child(grandson);
+	child->generate_child();
+	for(int i = 0 ;i < 100;i++){
+		grandson->add_customer_to_vertical_crp(concentration, g0, new_table_generated);
+	}
+	for(int i = 0 ;i < 100;i++){
+		child->remove_customer_from_vertical_crp(new_table_generated);
+		grandson->remove_customer_from_vertical_crp(new_table_generated);
+	}
+	assert(parent->_pass_count_v == 0);
+	assert(parent->_pass_count_h == 0);
+	assert(parent->_stop_count_v == 0);
+	assert(parent->_stop_count_h == 0);
+	for(int i = 0;i < parent->_children.size();i++){
+		assert(parent->_children[i]->_pass_count_v == 0);
+		assert(parent->_children[i]->_pass_count_h == 0);
+		assert(parent->_children[i]->_stop_count_v == 0);
+		assert(parent->_children[i]->_stop_count_h == 0);
+		assert(parent->_children[i]->_table_v->_num_customers == 0);
+		assert(parent->_children[i]->_table_h->_num_customers == 0);
+	}
+	for(int i = 0;i < child->_children.size();i++){
+		assert(child->_children[i]->_pass_count_v == 0);
+		assert(child->_children[i]->_pass_count_h == 0);
+		assert(child->_children[i]->_stop_count_v == 0);
+		assert(child->_children[i]->_stop_count_h == 0);
+		assert(child->_children[i]->_table_v->_num_customers == 0);
+		assert(child->_children[i]->_table_h->_num_customers == 0);
+	}
+}
+
+void test_remove_customer_to_horizontal_crp(){
+	Node* node = new Node();
+	double concentration = 0.2;
+	double g0 = 0.001;
+	bool new_table_generated;
+	for(int i = 0 ;i < 100;i++){
+		node->add_customer_to_horizontal_crp(concentration, g0, new_table_generated);
+	}
+	for(int i = 0 ;i < 100;i++){
+		node->remove_customer_from_horizontal_crp(new_table_generated);
+	}
+	for(int i = 0 ;i < 100;i++){
+		node->add_customer_to_horizontal_crp(concentration, g0, new_table_generated);
+		node->remove_customer_from_horizontal_crp(new_table_generated);
+	}
+	assert(node->_pass_count_v == 0);
+	assert(node->_pass_count_h == 0);
+	assert(node->_stop_count_v == 0);
+	assert(node->_stop_count_h == 0);
+	assert(node->_table_h->_num_customers == 0);
+}
+
+void test_remove_customer_to_horizontal_crp_recursively(){
+	Node* parent = new Node();
+	parent->generate_child();
+	parent->generate_child();
+	Node* child = new Node(parent);
+	parent->add_child(child);
+	parent->generate_child();
+	double concentration = 0.2;
+	double g0 = 0.001;
+	bool new_table_generated;
+	for(int i = 0 ;i < 100;i++){
+		child->add_customer_to_horizontal_crp(concentration, g0, new_table_generated);
+	}
+	child->generate_child();
+	child->generate_child();
+	Node* grandson = new Node(child);
+	child->add_child(grandson);
+	child->generate_child();
+	for(int i = 0 ;i < 100;i++){
+		grandson->add_customer_to_horizontal_crp(concentration, g0, new_table_generated);
+	}
+	for(int i = 0 ;i < 100;i++){
+		child->remove_customer_from_horizontal_crp(new_table_generated);
+		grandson->remove_customer_from_horizontal_crp(new_table_generated);
+	}
+	assert(parent->_pass_count_v == 0);
+	assert(parent->_pass_count_h == 0);
+	assert(parent->_stop_count_v == 0);
+	assert(parent->_stop_count_h == 0);
+	for(int i = 0;i < parent->_children.size();i++){
+		assert(parent->_children[i]->_pass_count_v == 0);
+		assert(parent->_children[i]->_pass_count_h == 0);
+		assert(parent->_children[i]->_stop_count_v == 0);
+		assert(parent->_children[i]->_stop_count_h == 0);
+		assert(parent->_children[i]->_table_v->_num_customers == 0);
+		assert(parent->_children[i]->_table_h->_num_customers == 0);
+	}
+	for(int i = 0;i < child->_children.size();i++){
+		assert(child->_children[i]->_pass_count_v == 0);
+		assert(child->_children[i]->_pass_count_h == 0);
+		assert(child->_children[i]->_stop_count_v == 0);
+		assert(child->_children[i]->_stop_count_h == 0);
+		assert(child->_children[i]->_table_v->_num_customers == 0);
+		assert(child->_children[i]->_table_h->_num_customers == 0);
+	}
+}
+
 int main(){
 	test_depth_v();
 	cout << "OK" << endl;
@@ -228,6 +366,14 @@ int main(){
 	test_add_customer_to_horizontal_crp();
 	cout << "OK" << endl;
 	test_add_customer_to_horizontal_crp_recursively();
+	cout << "OK" << endl;
+	test_remove_customer_to_vertical_crp();
+	cout << "OK" << endl;
+	test_remove_customer_to_vertical_crp_recursively();
+	cout << "OK" << endl;
+	test_remove_customer_to_horizontal_crp();
+	cout << "OK" << endl;
+	test_remove_customer_to_horizontal_crp_recursively();
 	cout << "OK" << endl;
 	return 0;
 }
