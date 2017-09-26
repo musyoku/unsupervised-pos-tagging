a20d8e69c5a029db3fffe94346e0e01f5e63597b
musyoku <musyoku@users.noreply.github.com>
2017-09-26 21:20:51 +0900

    :v:

--- infinite-tree-hmm/test/module_tests/node.cpp
+++ infinite-tree-hmm/test/module_tests/node.cpp
@@ -45,7 +45,11 @@ void test_add_customer_to_vertical_crp(){
 
 void test_add_customer_to_vertical_crp_recursively(){
 	Node* parent = new Node();
+	parent->generate_child();
+	parent->generate_child();
 	Node* child = new Node(parent);
+	parent->add_child(child);
+	parent->generate_child();
 	double concentration = 0.2;
 	double g0 = 0.001;
 	bool new_table_generated;
@@ -56,12 +60,66 @@ void test_add_customer_to_vertical_crp_recursively(){
 	assert(child->_pass_count_h == 0);
 	assert(child->_stop_count_v == 100);
 	assert(child->_stop_count_h == 0);
-	assert(child->_table_v->_num_customers == 100);
 	assert(parent->_pass_count_v == 100);
 	assert(parent->_pass_count_h == 0);
 	assert(parent->_stop_count_v == 0);
 	assert(parent->_stop_count_h == 0);
-	assert(parent->_table_v->_num_customers == 0);
+	assert(parent->_children[0]->_pass_count_v == 0);
+	assert(parent->_children[0]->_pass_count_h == 0);
+	assert(parent->_children[0]->_stop_count_v == 0);
+	assert(parent->_children[0]->_stop_count_h == 0);
+	assert(parent->_children[1]->_pass_count_v == 0);
+	assert(parent->_children[1]->_pass_count_h == 0);
+	assert(parent->_children[1]->_stop_count_v == 0);
+	assert(parent->_children[1]->_stop_count_h == 0);
+	assert(parent->_children[3]->_pass_count_v == 0);
+	assert(parent->_children[3]->_pass_count_h == 0);
+	assert(parent->_children[3]->_stop_count_v == 0);
+	assert(parent->_children[3]->_stop_count_h == 0);
+	child->generate_child();
+	child->generate_child();
+	Node* grandson = new Node(child);
+	child->add_child(grandson);
+	child->generate_child();
+	for(int i = 0 ;i < 100;i++){
+		grandson->add_customer_to_vertical_crp(concentration, g0, new_table_generated);
+	}
+	assert(grandson->_pass_count_v == 0);
+	assert(grandson->_pass_count_h == 0);
+	assert(grandson->_stop_count_v == 100);
+	assert(grandson->_stop_count_h == 0);
+	assert(child->_pass_count_v == 100);
+	assert(child->_pass_count_h == 0);
+	assert(child->_stop_count_v == 100);
+	assert(child->_stop_count_h == 0);
+	assert(parent->_pass_count_v == 200);
+	assert(parent->_pass_count_h == 0);
+	assert(parent->_stop_count_v == 0);
+	assert(parent->_stop_count_h == 0);
+	assert(parent->_children[0]->_pass_count_v == 0);
+	assert(parent->_children[0]->_pass_count_h == 0);
+	assert(parent->_children[0]->_stop_count_v == 0);
+	assert(parent->_children[0]->_stop_count_h == 0);
+	assert(parent->_children[1]->_pass_count_v == 0);
+	assert(parent->_children[1]->_pass_count_h == 0);
+	assert(parent->_children[1]->_stop_count_v == 0);
+	assert(parent->_children[1]->_stop_count_h == 0);
+	assert(parent->_children[3]->_pass_count_v == 0);
+	assert(parent->_children[3]->_pass_count_h == 0);
+	assert(parent->_children[3]->_stop_count_v == 0);
+	assert(parent->_children[3]->_stop_count_h == 0);
+	assert(child->_children[0]->_pass_count_v == 0);
+	assert(child->_children[0]->_pass_count_h == 0);
+	assert(child->_children[0]->_stop_count_v == 0);
+	assert(child->_children[0]->_stop_count_h == 0);
+	assert(child->_children[1]->_pass_count_v == 0);
+	assert(child->_children[1]->_pass_count_h == 0);
+	assert(child->_children[1]->_stop_count_v == 0);
+	assert(child->_children[1]->_stop_count_h == 0);
+	assert(child->_children[3]->_pass_count_v == 0);
+	assert(child->_children[3]->_pass_count_h == 0);
+	assert(child->_children[3]->_stop_count_v == 0);
+	assert(child->_children[3]->_stop_count_h == 0);
 }
 
 void test_add_customer_to_horizontal_crp(){
@@ -120,16 +178,6 @@ void test_add_customer_to_horizontal_crp_recursively(){
 	for(int i = 0 ;i < 100;i++){
 		grandson->add_customer_to_horizontal_crp(concentration, g0, new_table_generated);
 	}
-	cout << child->_pass_count_v << endl;
-	cout << child->_pass_count_h << endl;
-	cout << child->_stop_count_v << endl;
-	cout << child->_stop_count_h << endl;
-	cout << child->_table_h->_num_customers << endl;
-	cout << parent->_pass_count_v << endl;
-	cout << parent->_pass_count_h << endl;
-	cout << parent->_stop_count_v << endl;
-	cout << parent->_stop_count_h << endl;
-	cout << parent->_table_h->_num_customers << endl;
 	assert(grandson->_pass_count_v == 0);
 	assert(grandson->_pass_count_h == 0);
 	assert(grandson->_stop_count_v == 0);
